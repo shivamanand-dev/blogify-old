99324917368c0383515610095176e29152fc2cbb
Shivam Anand <shivam_anand@outlook.com>
2023-04-12 14:30:31 +0530

    initialize

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+import { Container, NoSsr } from "@mui/material";
+import { ThemeProvider } from "styled-components";
+
+// import Navbar from "@/components/Navbar";
+import theme from "@/utils/Theme/theme";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <NoSsr>
+        {/* <Navbar /> */}
+        <div style={{ height: "64px" }}></div>
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </NoSsr>
+    </ThemeProvider>
+  );
 }
---
+++ src/utils/theme/theme.js
@@ -0,0 +1,60 @@
+export const COLORS = {
+  DARK_COLOR: "#3c3c3c",
+  LIGHTS_COLOR: "#eeeeee",
+};
+
+export const FONT_SIZES = {
+  EXTRA_SMALL_A: ".5rem", //8px
+  EXTRA_SMALL_B: ".57rem", //9.12px
+  EXTRA_SMALL_C: ".68rem", // 11px
+  EXTRA_SMALL_D: ".87rem", //13.97px
+  EXTRA_SMALL_E: ".75rem", //12px
+  EXTRA_SMALL_F: ".81rem", //12.96,
+  EXTRA_SMALL_G: ".875rem", //14px
+  SMALL_A: "1rem", //16px
+  SMALL_B: "1.25rem", //20px
+  SMALL_C: "1.5rem", //24px
+  SMALL_D: "1.75rem", //28px
+  SMALL_E: "1.4rem", //22.4px
+  MEDIUM_A: "2rem", //32px
+  MEDIUM_B: "2.33rem", //37.28px
+  MEDIUM_C: "2.67rem", //42.72px
+  MEDIUM_D: "2.5rem", //40px
+  LARGE_A: "3rem", //48px
+  LARGE_B: "4rem", //64px
+  SMALL_F: "1.125rem", //18px
+  EXTRA_SMALL_H: ".625rem", //10px
+};
+
+export const BORDER_RADIUS = {
+  EXTRA_SMALL_A: ".5rem",
+  EXTRA_SMALL_B: ".57rem",
+  EXTRA_SMALL_C: ".68rem",
+  EXTRA_SMALL_D: ".87rem",
+  EXTRA_SMALL_E: ".125rem",
+  EXTRA_SMALL_F: ".3125rem",
+  EXTRA_SMALL_G: ".625rem",
+  SMALL_A: "1rem",
+  SMALL_B: "1.25rem",
+  SMALL_C: "1.5rem",
+  SMALL_D: "1.75rem",
+  SMALL_E: "1.875rem",
+  MEDIUM_A: "2rem",
+  MEDIUM_B: "2.33rem",
+  MEDIUM_C: "2.67rem",
+  MEDIUM_D: "2.5rem",
+  LARGE_A: "3rem",
+  LARGE_B: "4rem",
+  LARGE_C: "3.5rem",
+  LARGE_D: "4.5rem",
+  LARGE_E: "3.125rem",
+  EXTRA_LARGE_A: "3.75rem",
+};
+
+const theme = {
+  COLORS,
+  FONT_SIZES,
+  BORDER_RADIUS,
+};
+
+export default theme;
