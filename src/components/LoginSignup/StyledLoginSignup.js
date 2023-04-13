58e1dd0f5483f02766fbfe038a12e62ebc7717ce
Shivam Anand <shivam_anand@outlook.com>
2023-04-13 23:02:38 +0530

    login sign up

---
+++ src/components/LoginSignup/StyledLoginSignup.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledLoginSignup = styled.section`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 450px;
+  margin: 0 auto;
+  .switchForm {
+    cursor: pointer;
+  }
+`;
+
+export default StyledLoginSignup;
