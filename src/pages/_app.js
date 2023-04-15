baa185114991f9802fb22bf3e2babf9f4ef3647c
Shivam Anand <shivam_anand@outlook.com>
2023-04-16 01:48:51 +0530

    removed persistor redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,28 +1,37 @@
 import "@/styles/globals.css";
 
 import { Container, NoSsr } from "@mui/material";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 import { ThemeProvider } from "styled-components";
 
-import store, { persistor } from "@/store/store";
+import store from "@/store/store";
+import { authApi } from "@/utils/firebase/auth";
 // import Navbar from "@/components/Navbar";
 import theme from "@/utils/Theme/theme";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const user = authApi.getUser();
+
+    if (!user) {
+      return router.push("/login");
+    }
+  }, []);
+
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor} loading={null}>
-        <ThemeProvider theme={theme}>
-          <NoSsr>
-            {/* <Navbar /> */}
-            <div style={{ height: "64px" }}></div>
-            <Container>
-              <Component {...pageProps} />
-            </Container>
-          </NoSsr>
-        </ThemeProvider>
-      </PersistGate>
+      <ThemeProvider theme={theme}>
+        <NoSsr>
+          {/* <Navbar /> */}
+          <div style={{ height: "64px" }}></div>
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </NoSsr>
+      </ThemeProvider>
     </Provider>
   );
 }
