7aba875cfe35d7797ef7f9981c186229e1b8d034
Shivam Anand <shivam_anand@outlook.com>
2023-04-21 01:20:55 +0530

    redux persist

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,35 @@
 import "@/styles/globals.css";
 
 import { Container, NoSsr } from "@mui/material";
+import { getAuth } from "firebase/auth";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { ThemeProvider } from "styled-components";
 
-import { AuthProvider } from "@/context/AuthContext";
-import store from "@/redux/store";
-// import Navbar from "@/components/Navbar";
+// import { AuthProvider } from "@/context/AuthContext";
+import { persistor, store } from "@/redux/store";
+import app from "@/utils/firebase";
 import theme from "@/utils/Theme/theme";
 
 export default function App({ Component, pageProps }) {
+  const auth = getAuth(app);
+  const router = useRouter();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (!user) {
+        router.push("/login");
+      }
+    });
+  }, []);
+
+  // console.log(userData);
   return (
-    <AuthProvider>
-      <Provider store={store}>
+    // <AuthProvider>
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
         <ThemeProvider theme={theme}>
           <NoSsr>
             {/* <Navbar /> */}
@@ -22,7 +39,8 @@ export default function App({ Component, pageProps }) {
             </Container>
           </NoSsr>
         </ThemeProvider>
-      </Provider>
-    </AuthProvider>
+      </PersistGate>
+    </Provider>
+    // </AuthProvider>
   );
 }
