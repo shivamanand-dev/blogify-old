0f0deb544d736ee983055c7f93308c08a22289b8
Shivam Anand <shivam_anand@outlook.com>
2023-04-23 20:51:11 +0530

    documents api

--- src/components/StyledPages/StyledProfile.js
+++ src/components/StyledPages/StyledProfile.js
@@ -2,4 +2,13 @@ import styled from "styled-components";
 
 export const StyledProfile = styled.section`
   margin-top: 2rem;
+  .mainContainer {
+    margin-top: 5rem;
+    justify-content: space-between;
+    flex-direction: row-reverse;
+    .sidebar {
+      max-width: 30%;
+      width: 100%;
+    }
+  }
 `;
