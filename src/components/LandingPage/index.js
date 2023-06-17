70b2837506cf4e4b95256057b23b684774bef4e5
Shivam <shivam@Shivams-MacBook-Pro.local>
2023-06-17 16:14:49 +0530

    landing page button

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,13 +1,21 @@
+import { useRouter } from "next/router";
+
 import { PrimaryButton } from "../Buttons";
 import { StyledLandingPage } from "./StyledLandingPage";
 
 function LandingPage() {
+  const router = useRouter();
   return (
     <StyledLandingPage>
       <div className="hero flex">
         <h1>Welcome to Our Blogging Community</h1>
         <h2>Join thousands of writers and readers around the world</h2>
-        <PrimaryButton buttonText="Join now" />
+        <PrimaryButton
+          buttonText="Join now"
+          onClick={() => {
+            router.push("/login");
+          }}
+        />
       </div>
     </StyledLandingPage>
   );
