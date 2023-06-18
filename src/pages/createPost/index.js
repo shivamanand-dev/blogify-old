0ba547b8b314f394ecbf99f325c682c6bd1808fa
Shivam <shivam@Shivams-MacBook-Pro.local>
2023-06-18 16:09:02 +0530

    blog posts UI

--- src/pages/createPost/index.js
+++ src/pages/createPost/index.js
@@ -11,6 +11,7 @@ import TextArea from "@/components/TextArea";
 import TextEditor from "@/components/TextEditor";
 import { setBlogs } from "@/redux/blogsSlice";
 import { userState } from "@/redux/userSlice";
+import { app_routes } from "@/utils/constants/app_constants";
 import { firestoreApi } from "@/utils/firebase/firestore";
 import { blogServices } from "@/utils/firebase/services/blogServices";
 
@@ -73,7 +74,7 @@ function CreatePost() {
 
       dispatch(setBlogs(blogsData));
 
-      router.push("/feed");
+      router.push(app_routes.explore);
     }
   };
 
