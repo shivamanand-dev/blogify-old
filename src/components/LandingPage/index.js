b491892d53fcfa40a84b39b5b61d3334ce91de7b
Shivam Anand <shivam_anand@outlook.com>
2023-06-25 00:46:49 +0530

    router on landing

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,13 +1,23 @@
+import { useRouter } from "next/router";
+
+import { app_routes } from "@/utils/constants/app_constants";
+
 import { PrimaryButton } from "../Buttons";
 import { StyledLandingPage } from "./StyledLandingPage";
 
 function LandingPage() {
+  const router = useRouter();
   return (
     <StyledLandingPage>
       <div className="hero flex">
         <h1>Welcome to Our Blogging Community</h1>
         <h2>Join thousands of writers and readers around the world</h2>
-        <PrimaryButton buttonText="Join now" />
+        <PrimaryButton
+          buttonText="Join now"
+          onClick={() => {
+            router.push(app_routes.login);
+          }}
+        />
       </div>
     </StyledLandingPage>
   );
