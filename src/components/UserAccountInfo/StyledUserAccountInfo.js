49f769c56e62e3f3a49ed47c5ee6a2e5e3e68dbf
Shivam <shivam@Shivams-MacBook-Pro.local>
2023-06-27 00:22:38 +0530

    Refactor

--- src/components/UserAccountInfo/StyledUserAccountInfo.js
+++ src/components/UserAccountInfo/StyledUserAccountInfo.js
@@ -4,7 +4,7 @@ export const StyledUserAccountInfo = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 1rem 2rem;
+  padding: 2rem;
   box-shadow: 0px 0px 30px 5px #415a77;
   border-radius: 15px;
   /* width: 100%; */
